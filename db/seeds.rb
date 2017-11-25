81d1660af6183321be07367cbc492e33ce8e3f6d
github username <github email address>
2017-11-24 20:21:48 -0500

    updated seeds 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,8 @@
 
 al = User.create(username:'Alfa315', first_name:'Al', last_name: 'Artesona', zip_code: 07452)
 
+jules = User.create(username:'j12', first_name:'Jules', last_name: "O'Hagan", zip_code: 07452)
+
 todo1 = Todo.create(user_id: 1, todo_name: 'Go to Movies', todo_type: 'entertainment', todo_description: 'hello, does this work', deadline: '12/15/2017')
+
+todo2 = Todo.create(user_id: 2, todo_name: 'Go to Store', todo_type: 'entertainment', todo_description: 'hello, does this work for second user', deadline: '01/01/2018')
